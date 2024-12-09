d615500d34070fd41bb26610e790398376003b6c
Chutung91 <chutung912001@gmail.com>
2024-12-09 10:28:58 +0700

    now

--- nhapmonhtml/bai7_week2/Khaibao_sudungbien.js
+++ nhapmonhtml/bai7_week2/Khaibao_sudungbien.js
@@ -1,13 +1,31 @@
-let i = 10;
-let f = 20.5;
-let b = true;
-let s = 'Hà Nội';
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
 
-document.write('i = ' + i);
-document.write('<br/>');
-document.write('f = ' + f);
-document.write('<br/>');
-document.write('b = ' + b);
-document.write('<br/>');
-document.write('s = ' + s);
-console.log(i);
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br>')
+document.write('s = ' + s)
+document.write('<br>')
+// bai1
+let width = 20
+let height = 10
+let area =width * height
+document.write('Area = ' + area)
+// bai2
+let a
+let bb
+function nhapa(){
+    a = prompt("nhập a")
+}
+function nhapb(){
+    bb = prompt("nhập b")
+}
+function kiemtra(){
+    let c = a%bb == 0;
+    alert(c)
+}
