9ddbd44ed8dc2abe6e2a1b643ea42c8ee8c21ebc
Chutung91 <chutung912001@gmail.com>
2024-12-10 08:42:55 +0700

    case

---
+++ nhapmonhtml/bai8_week2/thuchanh_switch_case/switchcase_longnhau.js
@@ -0,0 +1,11 @@
+var a=100
+var b=200
+
+switch(a){
+    case 100:
+        alert("a="+a)
+        switch(b){
+            case 200:
+                alert("b="+b)
+        }
+}
---
+++ nhapmonhtml/bai8_week2/thuchanh_switch_case/thuchanh_switch_case.js
@@ -0,0 +1,22 @@
+var opera = "/"
+
+var a = 6
+var b = 3
+
+switch(opera){
+    case "+":
+        alert("a + b =" +(a+b))
+        break
+    case "-":
+        alert("a - b =" +(a-b))
+        break
+    case "*":
+        alert("a * b =" +(a*b))
+        break
+    case "/":
+        alert("a / b =" +(a/b))
+        break
+    default:
+        alert("khong co phep toan")
+        break
+}
