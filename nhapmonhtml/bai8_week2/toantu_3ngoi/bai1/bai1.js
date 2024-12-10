6140f5f81d9c5deeba6c7efb1eea1c5b10a58ad4
Chutung91 <chutung912001@gmail.com>
2024-12-10 10:44:28 +0700

    12

---
+++ nhapmonhtml/bai8_week2/toantu_3ngoi/bai1/bai1.js
@@ -0,0 +1,5 @@
+let a = +prompt("nhap a")
+let b = +prompt("nhap b")
+
+kq = (a+b)<4 ? alert('Below') : alert('Over')
+
---
+++ nhapmonhtml/bai8_week2/toantu_3ngoi/bai2/bai2.js
@@ -0,0 +1,3 @@
+let tn = prompt("message",'')
+
+tn == 'Employee' ? alert('Hello') : ( tn == 'Director' ? alert('Greetings'): (tn == '') ? alert('No login') : alert(''))
