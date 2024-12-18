7b7175d3d4deb1cf177df6bb4e3f96cbbf37787c
Chutung91 <chutung912001@gmail.com>
2024-12-18 11:04:06 +0700

    taogoiham

---
+++ nhapmonhtml/bai14_week3/binhphuong_1so/binhphuong_1so.js
@@ -0,0 +1,7 @@
+function square(number) {
+    var result = number * number;
+    return result;
+}
+
+var squareOf2 = square(2);
+document.write('Square of 2: ' + squareOf2);
---
+++ nhapmonhtml/bai14_week3/thuchanh/thuchanh.js
@@ -0,0 +1,11 @@
+// ham khong co tham so
+function welcome() {
+    alert('Hello World!');
+}
+// ham co tham so
+
+function sum(a,b) {
+    return a + b;
+}
+let total = sum(2, 3);
+alert(total);
