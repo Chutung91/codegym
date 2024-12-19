1b32c8e557d4b97a03be9c46fba64c817f425e33
Chutung91 <chutung912001@gmail.com>
2024-12-19 15:21:55 +0700

    thuchanh

--- nhapmonhtml/bai14_week3/thuchanh/thuchanh.js
+++ nhapmonhtml/bai14_week3/thuchanh/thuchanh.js
@@ -1,11 +1,39 @@
-// ham khong co tham so
-function welcome() {
-    alert('Hello World!');
+// dien tich hinh tron khong co tham so
+function dientich1() {
+    return Math.PI * Math.pow(5, 2);
 }
-// ham co tham so
 
-function sum(a,b) {
-    return a + b;
+console.log(dientich1());
+
+// dien tich hinh tron co tham so
+function dientich2(x) {
+    return Math.PI * Math.pow(x, 2);
 }
-let total = sum(2, 3);
-alert(total);
+
+console.log(dientich2(5));
+
+// chu i hinh tron khong co tham so
+function chuvi1() {
+    return Math.PI * 2 * 5;
+}
+
+console.log(chuvi1());
+
+// chu vi hinh tron co tham so
+function chuvi2(x) {
+    return Math.PI * x * 2;
+}
+
+console.log(chuvi2(5));
+
+// bai5
+function dongthoi(x) {
+    let arr = [];
+    arr[0] = Math.PI * Math.pow(x, 2);
+    arr[1] = Math.PI * 2 * x;
+    return arr;
+}
+
+for (i = 0; i < dongthoi(5).length; i++) {
+    console.log(dongthoi(5)[i]);
+}
---
+++ nhapmonhtml/bai15_week3/ungdung_quanly_sanpham/quanly_sp.js
@@ -0,0 +1,38 @@
+let n = 0;
+let arrProduct = [];
+// in ra cac san pham
+function inRa() {
+    document.getElementById('soluong').innerHTML = n + ' products';
+    let hienThi;
+    hienThi = '<table>';
+    for (let i = 0; i < n; i++) {
+        hienThi = hienThi + '<tr>';
+        hienThi = hienThi + '<td>'+ arrProduct[i] +'</td>';
+        hienThi = hienThi + '<td>'+ '<input type="submit" value="Edit" onclick="Edit()">' +'</td>';
+        hienThi = hienThi + '<td>'+ '<input type="submit" value="Delete" onclick="Delete()">' +'</td>';
+        hienThi = hienThi + '</tr>';
+    }
+    hienThi = hienThi + '</table>';
+    document.getElementById('hienthi').innerHTML = hienThi;
+}
+// them san pham
+function addProduct() {
+    if (document.getElementById('productName').value != '') {
+        arrProduct[n] = document.getElementById('productName').value;
+        n = n + 1;
+    }
+    inRa();
+    document.getElementById('productName').value = '';
+}
+// sua san pham
+function Edit() {
+
+}
+// xoa san pham
+function Delete() {
+
+}
+
+
+
+
